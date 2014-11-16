2a2fd9e8d6e87a9df516f9e1581f280fdc84f1aa
francoiscoron <francoiscoron23@gmail.com>
2014-11-16 16:57:00 +0100

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,60 @@
-var gulp = require('gulp'),
+var gulp    = require('gulp'),
     plumber = require('gulp-plumber'),
-    jade = require('gulp-jade'),
-    sass = require('gulp-ruby-sass');
+    rimraf  = require('rimraf'),
+    imageop = require('gulp-image-optimization'),
+    connect = require('gulp-connect'),
+    jade    = require('gulp-jade'),
+    sass    = require('gulp-ruby-sass');
+
+// Clean dist folder
+gulp.task('clean', function (cb) {
+    rimraf('dist/', cb);
+});
 
 gulp.task('jade', function(){
     gulp.src('src/templates/*.jade')
         .pipe(plumber())
         .pipe(jade({pretty: true}))
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest('dist/'))
+        .pipe(connect.reload());
 });
 
 gulp.task('sass', function(){
     gulp.src('src/styles/app.scss')
         .pipe(plumber())
         .pipe(sass({
-            sourcemapPath: '../sass',
-            sourcemap: true
+            sourcemapPath: 'src/styles'
         }))
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('dist/css'))
+        .pipe(connect.reload());
 });
 
-gulp.task('watch', ['jade','sass'], function(){
-    gulp.watch('templates/**/*.jade', ['jade'])
-    gulp.watch('styles/**/*.scss', ['sass'])
+// Images
+gulp.task('images', function() {
+    gulp.src('src/images/**/**')
+        .pipe(imageop({
+            optimizationLevel: 5,
+            progressive: true,
+            interlaced: true
+        }))
+        .pipe(gulp.dest('dist/img/'));
 });
+
+gulp.task('server', function(){
+    connect.server({
+        root: 'dist',
+        port: 8000,
+        livereload: true
+    })
+})
+
+gulp.task('dist',['images','jade','sass'], function(){
+
+})
+
+gulp.task('watch', ['dist'], function(){
+    gulp.watch('src/templates/**/*.jade', ['jade'])
+    gulp.watch('src/styles/**/*.scss', ['sass'])
+});
+
+gulp.task('default', ['server','watch']);
